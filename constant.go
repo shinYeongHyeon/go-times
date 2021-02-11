d3437afe24365596f7f53e5559feced1abd39df1
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-02-11 18:46:47 +0900

    Add MonthMap Variable

--- constant.go
+++ constant.go
@@ -13,7 +13,7 @@ var DaysMap = map[time.Weekday]int{
 	time.Saturday:  6,
 }
 
-// MonthMap is dependent for time package
+// MonthMapByInt is dependent for time package
 var MonthMapByInt = map[int]time.Month{
 	1: time.January,
 	2: time.February,
@@ -27,4 +27,20 @@ var MonthMapByInt = map[int]time.Month{
 	10: time.October,
 	11: time.November,
 	12: time.December,
+}
+
+// MonthMap is dependent for time package
+var MonthMap = map[time.Month]int{
+	time.January: 1,
+	time.February: 2,
+	time.March: 3,
+	time.April: 4,
+	time.May: 5,
+	time.June: 6,
+	time.July: 7,
+	time.August: 8,
+	time.September: 9,
+	time.October: 10,
+	time.November: 11,
+	time.December: 12,
 }
